7f181b151546464a82f461b16d99c23e810ce700
Kushyk Roman01~ <zanko.cergey@gmail.com>
2024-10-22 13:12:50 +0300

    почав вичвати класи

---
+++ 3_lab/my_class.py
@@ -0,0 +1,9 @@
+class MySuperClass:
+    def __init__(self, surname, name, mark):
+        # в середині конструктора створюються атрибути
+        self.surname = surname
+        self.name = name
+        self.mark = mark
+
+def function_in_module():
+    pass
